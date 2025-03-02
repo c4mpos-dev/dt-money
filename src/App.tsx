c4d5d825e7124b69d7e4f51159412b9b464f18ea
c4mpos-dev <campos.eet@gmail.com>
2025-03-02 14:33:27 -0300

    Styled-components cfg

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import { ThemeProvider } from "styled-components"
 import { defaultTheme } from "./styles/themes/default";
+import { GLobalStyle } from "./styles/global";
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-    <h1>Hello World, DT Money here!</h1>
+      <GLobalStyle />
+      
+      <h1>Hello World, DT Money here!</h1>
     </ThemeProvider>
   );
 }
